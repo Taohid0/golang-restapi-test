cf6dbba5edfc332ad4cb861e364cb94e2d070c63
Taohidul Islam <taohidulii@gmail.com>
2019-03-09 12:58:24 +0600

    search by age,team id and team name added

--- api/search.go
+++ api/search.go
@@ -19,22 +19,38 @@ type playerData struct {
 
 func SearchRouter() http.Handler {
 	r := chi.NewRouter()
-	r.Get("/",handleGet)
+	r.Get("/",handleSearch)
 
 	return r
 }
 
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
+func handleSearch(w http.ResponseWriter, r *http.Request) {
 	team_id := r.URL.Query().Get("team")
 	age := r.URL.Query().Get("age")
-
+	team_name := r.URL.Query().Get("team_name")
+	fmt.Println(team_name)
 	db := database.ConnecToDB()
 
 	player := playerData{}
 	allPlayers := make([]playerData, 0)
+	queryString := "SELECT p.name,p.image,p.age,t.name,t.flag from team as t,player as p where p.team=t.id AND"
 
-	rows, err := db.Query("SELECT p.name,p.image,p.age,t.name,t.flag from team as t,player as p where p.age=? OR team=?",age,team_id)
+	//if team_id != "" || age!="" || team_name!=""{
+	//	queryString +=" where"
+	//}
+	if team_id!=""{
+		queryString+=" p.team="+team_id +" AND"// need to ask about it
+	}
+	if age!=""{
+		queryString+=" p.age<"+age+" AND"
+	}
+	if team_name!=""{
+		queryString+=fmt.Sprintf(" t.name LIKE %s AND","\"%b%\"")
+	}
+	queryString +=" 1"
+	fmt.Println(queryString)
+	rows, err := db.Query(queryString)
 
 	if err != nil {
 		fmt.Print(err)
